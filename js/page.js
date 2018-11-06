d94da2cd6f0f5230bab1c6810edbedcc8c934c43
ljr <ljr2014ncu@gmail.com>
2018-11-06 16:58:03 +0800

    动画效果更新

--- js/page.js
+++ js/page.js
@@ -10,6 +10,7 @@ $(function () {
     // $('.carousel_box li').eq($(this).index()).fadeIn().siblings().fadeOut();
     $('.carousel_box li').eq($(this).index()).addClass('active').siblings().removeClass('active');
   });
+
   function play() {
     if (i < 0) {
       i = $('.circle span').length - 1;
@@ -20,7 +21,6 @@ $(function () {
     $('.circle span').eq(i).addClass("active").siblings().removeClass("active");
     // $('.carousel_box li').eq(i).fadeIn().siblings().fadeOut();
     $('.carousel_box li').eq(i).addClass('active').siblings().removeClass('active');
-    console.log(i)
   }
 
   function time() {
@@ -36,4 +36,58 @@ $(function () {
       timer = setInterval(time, 3000);
     }
   );
+
+
+})
+
+$(function () {
+  function menuScroll(box) {
+    var oBox = $(box);
+    if (!oBox.length) return;
+
+    function scrllFun() {
+      var wtop = $(window).scrollTop();
+      if (wtop > 80) {
+        oBox.addClass('scoll_top');
+      } else {
+        oBox.removeClass('scoll_top');
+      }
+    }
+    $(window).scroll(function () {
+      scrllFun();
+    });
+  }
+  menuScroll('.navbar')
+})
+$(function () {
+  $('.navbar_link').on('click', function (e) {
+    var target = e.target
+    if (target.id) {
+      switch (target.id) {
+        case 'index':
+          $('html,body').animate({
+            scrollTop: 0
+          }, 600)
+          break;
+        case 'business':
+          $('html,body').animate({
+            scrollTop: $('.module2').offset().top
+          }, 600)
+          break;
+        case 'news':
+          $('html,body').animate({
+            scrollTop: $('.module3').offset().top + 90
+          }, 600)
+          break;
+        case 'join':
+          // alert('选择');
+          break;
+        case 'contact':
+          $('html,body').animate({
+            scrollTop: $('.bottom_box').offset().top
+          }, 600)
+          break;
+      }
+    }
+  })
 })
